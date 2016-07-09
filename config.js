25d9dcd825498342fd0fe3590bc254dbba9d72ac
guilam34 <guillaumelam34@gmail.com>
2016-07-09 15:39:45 -0700

    deploy config typo

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 //Deploy config
 module.exports.client_id = '71bd8a0397f04922af588c60bd44391b';
 module.exports.client_secret = 'f0e449cc7e014fe9844a94a9cc0370b4';
-module.host_url = 'https://obscure-inlet-83427.herokuapp.com/';
+module.exports.host_url = 'https://obscure-inlet-83427.herokuapp.com/';
 
 //Dev config
 // module.exports.client_id = 'ed3b512e06d94fdc94ee6b3581393e14';
