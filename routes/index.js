20df7a51e3038c1a71e00faf90667d560b96a0e3
guilam34 <guillaumelam34@gmail.com>
2016-07-08 20:29:23 -0700

    switch to deploy credentials

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,16 @@ var router = express.Router();
 var request = require('request');
 var querystring = require('querystring');
 
-var client_id = 'ed3b512e06d94fdc94ee6b3581393e14';
-var client_secret = 'b0505301d1dd4efa92c0ae4b1b037aae';
+//Deploy config
+var client_id = '71bd8a0397f04922af588c60bd44391b';
+var client_secret = 'f0e449cc7e014fe9844a94a9cc0370b4';
+var host_url = 'https://obscure-inlet-83427.herokuapp.com/';
 
-var host_url = 'http://localhost:3000/';
+//Dev config
+// var client_id = 'ed3b512e06d94fdc94ee6b3581393e14';
+// var client_secret = 'b0505301d1dd4efa92c0ae4b1b037aae';
+//var host_url = 'http://localhost:3000/';
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
